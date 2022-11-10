fda42df631082aaf7ba233d66c949f12304aeb6d
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-11-09 23:15:25 -0300

    feat: implements CreateEmployee use case

---
+++ src/domain/usecases/create-employee/index.js
@@ -0,0 +1,24 @@
+import { DuplicatedEmployeeError } from '../../errors'
+
+export class CreateEmployee {
+  #createEmployeeRepository = null
+  #getEmployeeRepository = null
+
+  constructor({ createEmployeeRepository, getEmployeeRepository }) {
+    this.#createEmployeeRepository = createEmployeeRepository
+    this.#getEmployeeRepository = getEmployeeRepository
+  }
+
+  async execute(employee) {
+    const employeeExists = await this.#getEmployeeRepository.get({
+      cpf: employee.cpf,
+    })
+
+    if (employeeExists) {
+      throw new DuplicatedEmployeeError(employeeExists.name)
+    }
+
+    const employeeCreated = await this.#createEmployeeRepository.create(employee)
+    return employeeCreated
+  }
+}
